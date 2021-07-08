e00c5ab35e24bd5a61896752acddcdc2f2c60dd2
Kelig MARTIN <kmartin@wedigital.garden>
2021-07-08 10:29:06 +0200

    add registry module

---
+++ registry/src/test/java/fr/esgi/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
