45ae3163b404656a1c4f6b57a66e954837160cb2
Kelig MARTIN <kmartin@wedigital.garden>
2021-07-09 11:10:50 +0200

    init microservices

---
+++ market/src/main/java/fr/esgi/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
+
+}
